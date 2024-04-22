29fd2b16d5239d9847961b71e035af5ee803bf02
SangHyeok0908 <tkdgur9799@gmail.com>
2024-04-22 12:20:05 +0900

    [REMOVE] 전체 코드 삭제, [ADD] 도서관 구상도안

--- 도서관/Library/Library/MainProgram.cs
+++ 도서관/Library/Library/MainProgram.cs
@@ -1,19 +1,17 @@
-﻿using Library.Controller;
-using Library.Utility;
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Library
 {
     public class MainProgram
     {
-
+        
         public static void Main(string[] args)
         {
-            Console.SetWindowSize(100, 35);
-            Console.CursorVisible = false;
 
-            ModeMenu controller = new ModeMenu();
-            controller.Run();
         }
     }
 }

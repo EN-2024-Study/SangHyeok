f971963db23214bb47a151ba665b209448d8d188
SangHyeok0908 <tkdgur9799@gmail.com>
2024-05-04 00:46:58 +0900

    [ADD] 검색 버튼과 뒤로가기 버튼을 눌렀을 때 panel이 바뀌는 기능

--- ImageSearch/src/Constants.java
+++ ImageSearch/src/Constants.java
@@ -2,4 +2,6 @@ public class Constants {
     public static final String TITLE = "Search Image";
     public static final String BUTTON_SEARCH = "검색";
     public static final String BUTTON_HISTORY = "기록 조회";
+    public static final String BUTTTON_GOBACK = "뒤로 가기";
+
 }
--- ImageSearch/src/MainFrame.java
+++ ImageSearch/src/MainFrame.java
@@ -2,22 +2,81 @@ import javax.swing.*;
 import java.awt.*;
 
 public class MainFrame extends JFrame {
+
+    private SearchPanel searchPanel;
+    private SearchedPanel searchedPanel;
+
     public MainFrame() {
+        this.searchPanel = new SearchPanel();
+        this.searchedPanel = new SearchedPanel();
+
         setTitle(Constants.TITLE);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        MainPanel mainPanel = new MainPanel();
-        Container pane = getContentPane();
-        pane.setLayout(new FlowLayout());
-        pane.add(mainPanel.getSearchPanel());
-
+        setLayout(new FlowLayout());
+        add(searchPanel);
         setSize(600, 600);
         setVisible(true);
     }
 
+    private class SearchPanel extends JPanel {
+
+        private SearchPanel() {
+            setLayout(new FlowLayout());
+            add(getTextField());
+            add(getSearchButton());
+            add(getHistoryButton());
+        }
+    }
+
+    private class SearchedPanel extends JPanel {
+
+        private SearchedPanel() {
+            setLayout(new FlowLayout());
+            add(getTextField());
+            add(getSearchButton());
+            add(getGoBackButton());
+        }
+    }
+
+
+    private JTextField getTextField() {
+        JTextField textField = new JTextField(15);
+//        textField.addActionListener(e -> {
+//        });
+        return textField;
+    }
+
+    private JButton getSearchButton() {
+        JButton searchButton = new JButton(Constants.BUTTON_SEARCH);
+        searchButton.addActionListener(e -> {
+            remove(searchPanel);
+            add(searchedPanel);
+            revalidate();
+            repaint();
+        });
+        return searchButton;
+    }
+
+    private JButton getHistoryButton() {
+        JButton historyButton = new JButton(Constants.BUTTON_HISTORY);
+//        historyButton.addActionListener(e -> {
+//        });
+        return historyButton;
+    }
+
+    private JButton getGoBackButton() {
+        JButton goBackButton = new JButton(Constants.BUTTTON_GOBACK);
+        goBackButton.addActionListener(e -> {
+            remove(searchedPanel);
+            add(searchPanel);
+            revalidate();
+            repaint();
+        });
+        return goBackButton;
+    }
 
     public static void main(String[] args) {
-        MainFrame mainFrame = new MainFrame();
-
+        new MainFrame();
     }
 }
